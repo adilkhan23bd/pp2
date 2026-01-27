089a0937b9c942e353011ffd66ab6cd9c869e890
adilkhan23bd <adilkhan20080126@gmail.com>
2026-01-27 14:07:07 +0500

    Added

--- exmple1/1.py
+++ exmple1/1.py
@@ -1,1 +1,10 @@
-print("Hello")
+where = input("Go left or right?")
+count = 0
+while where  == "right":
+    count += 1
+    where = input("Go left or right?")
+
+if count > 2:
+    print(":(")
+
+print("You got out")
