0332c96430df803397b0fe4dd422e2e7010dd13f
adilkhan23bd <adilkhan20080126@gmail.com>
2026-01-30 00:43:15 +0500

    Added

---
+++ 2.py
@@ -0,0 +1,3 @@
+print("Hello, World!")
+print("Tair")
+print("3")
--- exmple1/1.py
+++ exmple1/1.py
@@ -1,10 +1,1 @@
-where = input("Go left or right?")
-count = 0
-while where  == "right":
-    count += 1
-    where = input("Go left or right?")
-
-if count > 2:
-    print(":(")
-
-print("You got out")
+print("Hello, World!")
