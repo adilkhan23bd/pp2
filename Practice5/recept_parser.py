4a9faa64c647be01cd1fc58e3901768c8b9de26b
adilkhan23bd <adilkhan20080126@gmail.com>
2026-03-06 14:51:24 +0500

    Add Practice5 - Python RegEx and receipt parsing examples

---
+++ Practice5/recept_parser.py
@@ -0,0 +1,29 @@
+import re
+
+with open(r"C:\Users\Адильхан\Desktop\pp2\work\Practice5/raw.txt", "r", encoding="utf-8") as file:
+    text = file.read()
+
+prices = re.findall(r'\d+\s?\d*,\d{2}', text)
+
+products = re.findall(r'\d+\.\n([^\n]+)', text)
+
+total = re.search(r'ИТОГО:\n([\d\s,]+)', text)
+total_amount = total.group(1) if total else "Not found"
+
+datetime = re.search(r'Время:\s*(\d{2}\.\d{2}\.\d{4}\s*\d{2}:\d{2}:\d{2})', text)
+datetime_value = datetime.group(1) if datetime else "Not found"
+
+payment = re.search(r'(Банковская карта|Наличные)', text)
+payment_method = payment.group(1) if payment else "Not found"
+
+print("Products:")
+for p in products:
+    print("-", p)
+
+print("\nPrices:")
+for pr in prices:
+    print(pr)
+
+print("\nTotal:", total_amount)
+print("Date and Time:", datetime_value)
+print("Payment method:", payment_method)
